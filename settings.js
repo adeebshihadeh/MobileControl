591bea7670d35713290127f7b848997a3d86a339
Adeeb Shihadeh <quillford@users.noreply.github.com>
2016-05-18 22:56:30 -0700

    initial commit

---
+++ settings.js
@@ -0,0 +1,24 @@
+var settings = {
+    "jog": {
+        "xyFeedrate": 3000,
+        "zFeedrate": 200
+    },
+    "custom": [
+        {
+            "title": "Fan On",
+            "command": "M106" 
+        },
+        {
+            "title": "Fan Off",
+            "command": "M107" 
+        },
+        {
+            "title": "Hotend Fan On",
+            "command": "M42" 
+        },
+        {
+            "title": "Hotend Fan Off",
+            "command": "M43" 
+        }
+    ]
+};
