3463ec5b6c8d1a6d207979fe7a366abb1533b2c2
Adeeb Shihadeh <quillford@users.noreply.github.com>
2016-05-21 11:38:05 -0700

    enhanced structure

---
+++ machine.js
@@ -0,0 +1,67 @@
+var machine = {
+    ip: localStorage.getItem("ip"),
+    apikey: localStorage.getItem("apikey"),
+    getIp: function(){
+        return ip;
+    },
+    connect: function(){
+        this.socket = new WebSocket("ws:///"+this.ip+"/sockjs/websocket");
+        
+        this.socket.onopen = function(){
+            console.log("socket opened");  
+        };
+        
+        this.socket.onmessage = function(){
+            console.log("you have mail");
+        };
+        
+        this.socket.onclose = function(){
+            window.location.href = "reconnect.html"; 
+        };
+    },
+    setIp: function(ip){
+        this.ip = ip;
+        localStorage.setItem("ip", this.ip);
+    },
+    sendCommand: function(command){
+        $.ajaxSetup({headers:{"X-Api-Key" : this.apikey}});
+        $.ajax({
+            url: "http://"+this.ip+"/api/printer/command",
+            contentType: "application/json",
+            data: JSON.stringify({"command": command}),
+            method: "POST"
+        });
+    },
+    sendCommands: function(commands){
+        $.ajaxSetup({headers:{"X-Api-Key" : this.apikey}});
+        $.ajax({
+            url: "http://"+this.ip+"/api/printer/command",
+            contentType: "application/json",
+            data: JSON.stringify({"commands": commands}),
+            method: "POST"
+        });
+    },
+    jogAxis: function(axis, direction, amount, feedrate){
+        this.sendCommands(["G91", "G1 " + axis + increment*direction + " F" + feedrate, "G90"]);
+    },
+    home: function(axis){
+        this.sendCommand("G28" + axis);
+    },
+    getFiles: function(){
+        $.ajaxSetup({headers:{"X-Api-Key" : this.apikey}, async: false});
+        $.getJSON("http://"+this.ip+"/api/files", function(e){
+            console.log(e);
+            
+            return e.files;
+        });
+    },
+    printFile: function(filename, local){
+        $.ajaxSetup({headers:{"X-Api-Key" : this.apikey}});
+        $.ajax({
+            url: "http://"+this.ip+"/api/files/"+(local ? "local" : "sd")+"/"+filename,
+            contentType: "application/json",
+            data: JSON.stringify({"command": "select", "print": true}),
+            method: "POST"
+        });
+    }
+};
